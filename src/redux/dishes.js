e5a21f7b279141c873aac7d0b6fb7827b7d8d208
rafirm29 <13519154@std.stei.itb.ac.id>
2021-09-04 13:35:37 +0700

    Combining reducers

---
+++ src/redux/dishes.js
@@ -0,0 +1,9 @@
+import { DISHES } from '../shared/dishes';
+
+export const Dishes = (state = DISHES, action) => {
+  switch (action.type) {
+    case value:
+    default:
+      return state;
+  }
+};
---
+++ src/redux/promotions.js
@@ -0,0 +1,9 @@
+import { PROMOTIONS } from '../shared/promotions';
+
+export const Promotions = (state = PROMOTIONS, action) => {
+  switch (action.type) {
+    case value:
+    default:
+      return state;
+  }
+};
